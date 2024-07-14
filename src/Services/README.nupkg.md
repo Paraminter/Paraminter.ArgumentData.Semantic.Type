0f11b1c02ba9eebb26bf87387b9da558680c6e32
ErikWe <erikwestberg7@gmail.com>
2024-07-14 02:45:11 +0200

    Dont provide factory for data type

--- src/Services/README.nupkg.md
+++
@@ -1,5 +0,0 @@
-# Paraminter.Arguments.Semantic.Type.Services
-
-Provides general implementations of services related to `Paraminter` semantic type argument data.
-
-See [GitHub](https://github.com/Paraminter/Paraminter.Arguments.Semantic.Type) for more information.
--- tests/unit/Services/SemanticTypeArgumentDataFactory/IFixture.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Paraminter.Arguments.Semantic.Type;
-
-using Paraminter.Arguments.Semantic.Type.Queries;
-using Paraminter.Queries.Handlers;
-
-internal interface IFixture
-{
-    public abstract IQueryHandler<IGetSemanticTypeArgumentDataQuery, ISemanticTypeArgumentData> Sut { get; }
-}
